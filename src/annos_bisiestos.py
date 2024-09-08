744869f85b50f12c3f8b2ce0be2e7e7a34164102
sam <notme@sam.xbz>
2024-09-07 23:57:57 -0500

    Se agregan los ejercicios annosbisiestos y division

--- src/annos_bisiestos.py
+++ src/annos_bisiestos.py
@@ -1,10 +1,11 @@
 def evaluar(anno):
-    # TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-    return "";
+    if (anno % 4 == 0 and anno % 100 != 0) or (anno % 400 == 0):
+        return f"(anno) es bisiesto"
+    else:
+        return f"{anno} no es bisiesto"
 
 if __name__ == '__main__':
     print("Año:", end="")
     anno = int(input())
-
     respuesta = evaluar(anno)
     print(respuesta)
